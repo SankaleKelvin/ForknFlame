b3ff8d9e691703bb1675d84c9e2305fd0aa62577
Kelvin Sankale <kelvin.sankale@strathmore.edu>
2025-10-14 12:21:20 +0300

    CRUD Functions for Role

--- app/Http/Controllers/RoleController.php
+++ app/Http/Controllers/RoleController.php
@@ -43,4 +43,55 @@ class RoleController extends Controller
             ], 500);
         }
     }
+
+    public function show($id){
+        $role = Role::findOrFail($id);
+        if($role){
+            return response()->json([
+                'Role'=>$role
+            ], 200);
+        }
+        else{
+            return "Role not found.";
+        }
+    }
+
+    public function update(Request $request, $id){
+        $validated=$request->validate([
+            'name'=>'required | string',
+            'slug'=>'required| string | unique:roles,slug'
+        ]);
+
+        $role = Role::findOrFail($id);
+        $role->name = $validated['name'];
+        $role->slug = $validated['slug'];
+
+        try{
+            $role->save();
+            return response()->json([
+                'Role'=>$role,
+            ], 200);
+        }
+        catch(\Exception $exception){
+            return response()->json([
+                'Error'=>"Failed to Update Role",
+            ], 500);
+        }
+
+    }
+
+    public function delete($id){
+        $role = Role::findOrFail($id);
+        
+        try{
+            $role->destroy($id);
+            return "Role Deleted Successfully!";
+        }
+        catch(\Exception $exception){
+            return response()->json([
+                'Error'=>"Failed to Delete Role", $exception->getMessage()
+            ], 500);
+        }
+
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -7,6 +7,9 @@ use Illuminate\Support\Facades\Route;
 //Public Routes
 Route::post('/saveRole', [RoleController::class, 'store']);
 Route::get('/getRoles', [RoleController::class, 'index']);
+Route::get('/getRole/{id}', [RoleController::class, 'show']);
+Route::post('/updateRole/{id}', [RoleController::class, 'update']);
+Route::delete('/deleteRole/{id}', [RoleController::class, 'delete']);
 
 //Protected Routes
 Route::middleware(['auth:sanctum'])->group(function () {});
